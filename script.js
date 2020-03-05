7d43c6d96fea2241102456f40de2f3f84054fd68
AliceRolim <alicevrolim@edu.unifor.br>
2020-03-05 22:39:17 +0000

    criado

---
+++ script.js
@@ -0,0 +1,14 @@
+function CriaTabela(){
+tam=4;
+var table= document.getElementById("table");
+for(var i=0;i<tam;i++){
+var linha =table.insertRow(i);
+var coluna1=row.insertCell(0);
+coluna1.innerHTML = i;
+var coluna2=row.insertCell(1);
+coluna2.innerHTML = "palavra"
+}
+
+
+
+}
