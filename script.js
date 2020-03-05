fccb2c8e16f9af7d198c38173d866df259d6854a
André Rodrigues Melo <andremelo13@gmail.com>
2020-03-05 23:13:17 +0000

    Remover atributo onchange do input de arquivos

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function CriaTabela(){
-tam=4;
-var table= document.getElementById("tabela");
-for(var i=0;i<tam;i++){
-var row =table.insertRow(i);
-var coluna1=row.insertCell(0);
-coluna1.innerHTML = i;
-var coluna2=row.insertCell(1);
-coluna2.innerHTML = "palavra"
-}
+var words = ''
+var fileInput = document.querySelector("#arq")
+
+async function CriaTabela() {
+  words = await fileInput.files[0].text()  
+  console.log(words)
+  
+  tam = 4;
+  var table = document.getElementById("tabela");
+  for (var i = 0; i < tam; i++) {
+    var row = table.insertRow(i);
+    var coluna1 = row.insertCell(0);
+    coluna1.innerHTML = i;
+    var coluna2 = row.insertCell(1);
+    coluna2.innerHTML = "palavra"
+  }
 
 }
