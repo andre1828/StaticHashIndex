206ee4f438cb72ca5dba74bd7141c2e67e2a6d3f
AliceRolim <alicevrolim@edu.unifor.br>
2020-03-05 22:42:48 +0000

    ageitou o script

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function CriaTabela(){
 tam=4;
-var table= document.getElementById("table");
+var table= document.getElementById("tabela");
 for(var i=0;i<tam;i++){
-var linha =table.insertRow(i);
+var row =table.insertRow(i);
 var coluna1=row.insertCell(0);
 coluna1.innerHTML = i;
 var coluna2=row.insertCell(1);
 coluna2.innerHTML = "palavra"
 }
-
-
 
 }
