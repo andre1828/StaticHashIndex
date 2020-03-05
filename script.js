b5c60909ba67b2185b94c6ef1f86be6cf87b7797
AliceRolim <alicevrolim@edu.unifor.br>
2020-03-05 23:43:22 +0000

    adicionou as palavras na tabela

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-function CriaTabela(){
-var tam =document.getElementById("tamanho").value;
-var arq =document.getElementById("arq");
+var words = '';
+var fileInput = document.querySelector("#arq");
 
-document.getElementById("criar").disabled = true;
+async function CriaTabela() {
+  words = await fileInput.files[0].text();
+  words = words.split("\n");
+  console.log(words);
+
+var tam =document.getElementById("tamanho").value;
+
+//document.getElementById("criar").disabled = true;
 var table= document.getElementById("tabela");
 
-
-
-
-console.log(res);
 for(var i=0;i<tam;i++){
 var row =table.insertRow(i);
 var coluna1=row.insertCell(0);
 coluna1.innerHTML = i;
 var coluna2=row.insertCell(1);
-coluna2.innerHTML = arq;
+coluna2.innerHTML = words[i];
 }
 
 }
