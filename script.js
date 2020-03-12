25d9dc5e5ff2c70f1e920ee00a1eeb435ec2aff0
AliceRolim <alicevrolim@edu.unifor.br>
2020-03-12 22:36:23 +0000

    12/03

--- script.js
+++ script.js
@@ -4,19 +4,81 @@ var fileInput = document.querySelector("#arq");
 async function CriaTabela() {
   words = await fileInput.files[0].text();
   words = words.split("\n");
-  console.log(words);
 
 var tam =document.getElementById("tamanho").value;
 
 //document.getElementById("criar").disabled = true;
 var table= document.getElementById("tabela");
 
+  var tablehash= document.getElementById("tabelahash");
+
+var keys= new Array();
+var hashv = new Array();
+var tab = new Object();
+
+
+for(var j=0;j<words.length;j++){
+var p=Math.floor(Math.random() * words.length);
+
+ if(!keys.includes(p)){
+            keys[j]=p;
+          }else{
+        j--;
+          }  }
+
+
+tab.key= keys;
+tab.tupla= words;
+
+/*
 for(var i=0;i<words.length;i++){
 var row =table.insertRow(i);
 var coluna1=row.insertCell(0);
-coluna1.innerHTML = i;
+coluna1.innerHTML = keys[i];
 var coluna2=row.insertCell(1);
 coluna2.innerHTML = words[i];
-}
+*/
+
+
+
+
+
+
+
+for(var l=0;l<words.length;l++){
+var row =table.insertRow(l);
+var coluna1=row.insertCell(0);
+coluna1.innerHTML =tab.key[l];
+var coluna2=row.insertCell(1);
+coluna2.innerHTML =tab.tupla[l];
 
 }
+
+for(var a=0;a<words.length;a++){
+var row2 =tablehash.insertRow(a);
+var coluna12=row2.insertCell(0);
+coluna12.innerHTML =hash(tab.key[a]);
+var coluna32=row2.insertCell(1);
+coluna32.innerHTML =tab.key[a];
+var coluna22=row2.insertCell(2);
+coluna22.innerHTML =tab.tupla[a];
+
+
+}
+
+
+
+
+}
+
+
+
+function hash(k){
+m= 3;
+return k%m;
+///h(k)=k mod m
+//usar o conjunto set
+
+}
+
+
